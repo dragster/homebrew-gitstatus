d5677dfa05dee20f935245e2206818b4ce1815e1
D <tomo@revzero.dev>
2023-12-31 06:53:29 +0900

    v0.1

---
+++ Casks/gitstatus.rb
@@ -0,0 +1,11 @@
+cask "gitstatus" do
+  version "0.1"
+  sha256 :no_check
+
+  url "https://www.dragster.dev/gitstatus/gitstatus-#{version}.dmg"
+  name "Git Status"
+  desc "Git status software"
+  homepage "https://www.dragster.dev/gitstatus/"
+
+  app "Git Status.app"
+end
